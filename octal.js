0176f5a7bb5065424364535c2664fb985cbff3cd
yashvii234 <yashvikalola2003@gmail.com>
2022-06-18 11:37:47 +0530

    octal file updated

--- octal.js
+++ octal.js
@@ -6,3 +6,12 @@ let answer = (parseInt(numberone, 8) + parseInt(numbertwo, 8)).toString(8);
 
 console.log(answer);
 
+weiusdfhnvkjefdsn giergvoerhb efhdikjewl;WebGLBuffergvaersgv
+
+e
+rasfbgveafpmb;wrsdz
+visualViewportrg
+vxc
+WebGLBuffergvaersgvbve
+4rwdg
+re
